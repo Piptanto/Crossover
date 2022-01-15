0b03c60bd50fbe1c4a73406bd9706b4f0ec8e45c
Elisabeth Ege <piptanto@gmail.com>
2022-01-15 11:39:06 +0100

    whatever

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express()
-// const cors = require("cors");
+const cors = require("cors");
 
 
 // Import DB
